83d5dc1d8ddf6b39f938a76a00786da9ce7fd05e
Gleb1C <pan8gleb@gmail.com>
2021-12-13 21:29:29 +0300

    les8_kvadro_line

--- Snake3/Snake3/Program.cs
+++ Snake3/Snake3/Program.cs
@@ -5,16 +5,27 @@ namespace Snake3
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
-            line.Drow();
-            VerticalLine lineV = new VerticalLine(9, 6, 15, '$');
-            lineV.Drow();
+            
+
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0,24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            rightLine.Drow();
+
+
+            Point p = new Point(4, 5, '*');
+            //HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            //line.Drow();
+            //VerticalLine lineV = new VerticalLine(9, 6, 15, '$');
+            //lineV.Drow();
 
 
 
